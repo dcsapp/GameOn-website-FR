9f21d58bf45d0a06ac7b39b27cd7f77f79338491
mG <testltde95@gmail.com>
2023-09-25 18:19:31 +0200

    first step in form handling

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,42 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal without update
+function closeModal() {
+  modalbg.style.display = none;
+}
 
+/* 
+const form = document.getElementById('reserve');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  validate();
+}) */
+
+function validate() {
+  /* var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/; */
+  var firstname = document.getElementById("first").value;
+  var lastname = document.getElementById("last").value;
+  var email = document.getElementById("email").value;
+  var birthdate = document.getElementById("birthdate").value;
+  var quantity = document.getElementById("quantity").value;
+  var location = document.getElementById("location").value;
+
+  if (firstname.length > 2) {
+    /* alert(firstname); */
+    /*  this.data-error-visible : true; */
+    /*  data-error-visible="true" */
+    /* text-control.value = 'toto'; */
+    /*   ::after */
+  } else {
+    /* firstname.classListAdd('formData[data-error]::after')
+
+   firstname.classListAdd('formData[data-error-visible="true"]::after');
+   firstname.classListAdd('text-control'); */
+
+    var toto = this.label;
+    alert('toto');
+    /* 
+   alert('Error must be more than 2 caracters'); */
+  }
+}
